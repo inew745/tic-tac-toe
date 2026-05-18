578a52ac3cbf684ec5c55f97d2c77580e8469350
inew745 <liambase745@gmail.com>
2026-05-18 16:45:04 +0300

    Moio Coder: update src/Square.tsx

---
+++ src/Square.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type SquareProps = {
+  value: string | null;
+  onClick: () => void;
+};
+
+const Square: React.FC<SquareProps> = ({ value, onClick }) => (
+  <button className="square" onClick={onClick}>
+    {value}
+  </button>
+);
+
+export default Square;
